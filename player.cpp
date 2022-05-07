4360d566296d4f2c2607cc06fe2c60fe8ad74658
ezaldensaadi <azosmn@hotmail.com>
2022-05-08 01:49:48 +0300

    changes

--- player.cpp
+++ player.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include "player.h"
 
+
+player::player(const char*Name,int Force,int HP):
+m_name(Name),m_force(Force),m_maxHp(HP),m_hp(HP),m_coins(0),m_level(1)
+{}
+player& player::levelUp() {
+    if(m_level==10){
+        return *this;
+    }
+    m_level++;
+    return *this;
+}
+int player::getLevel() const {
+    return m_level;
+}
+player& player::buff(int buff) {
+    m_force+=buff;
+    return *this;
+}
+player& player::damage(int damage) {
+    if((m_hp-damage)<0){
+        m_hp=0;
+    } else{
+        m_hp-=damage;
+    }
+    return *this;
+}
+bool player::pay(int amount) {
+    if(m_coins-amount<0){
+        return false;
+    } else{
+        m_coins-=amount;
+        return true;
+    }
+
+}
--- player.h
+++ player.h
@@ -1,1 +1,34 @@
+//
+// Created by Yosef Sabbah on 08/05/2022.
+//
 
+#ifndef PLAYER_PLAYER_H
+#define PLAYER_PLAYER_H
+class player{
+    const char * m_name;
+    int m_force;
+    int m_hp;
+    int m_level;
+    unsigned int m_coins;
+    int m_maxHp;
+public:
+    player(const char*Name,int Force=5,int HP=100);
+    ~player()=default;
+    player(const player&)=default;
+    void printInfo() const;
+    player& levelUp();
+    int getLevel()const;
+    player& buff(int buff);
+    player& heal(int heal);
+    player& damage(int damage);
+    bool isKnockedOut() const;
+    player& addCoins(int coin);
+    bool pay(int amount);
+    int fetAttackStrength() const;
+
+
+
+
+
+};
+#endif //PLAYER_PLAYER_H
